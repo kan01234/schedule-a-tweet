ced3446343e380eb65bb178fdf78a458025a5d9c
kan01234 <safghjkl@gmail.com>
2019-05-27 19:22:20 +0800

    add twitter config

--- src/main/java/com/dotterbear/file/upload/SpringFileUploadApplication.java
+++ src/main/java/com/dotterbear/file/upload/SpringFileUploadApplication.java
@@ -2,8 +2,10 @@ package com.dotterbear.file.upload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringFileUploadApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/dotterbear/file/upload/config/Twitter4jConfig.java
@@ -0,0 +1,27 @@
+package com.dotterbear.file.upload.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+@Configuration
+public class Twitter4jConfig {
+
+  @Bean
+  public Twitter getTwitter(@Value("${com.dotterbear.twitter.consumer.key}") String consumerKey,
+      @Value("${com.dotterbear.twitter.consumer.secret}") String consumerSecret,
+      @Value("${com.dotterbear.twitter.access.token}") String accessToken,
+      @Value("${com.dotterbear.twitter.access.secret}") String accessSecret) {
+    ConfigurationBuilder cb = new ConfigurationBuilder();
+    cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
+        .setOAuthConsumerSecret(consumerSecret)
+        .setOAuthAccessToken(accessToken)
+        .setOAuthAccessTokenSecret(accessSecret);
+    TwitterFactory tf = new TwitterFactory(cb.build());
+    return tf.getInstance();
+  }
+
+}
