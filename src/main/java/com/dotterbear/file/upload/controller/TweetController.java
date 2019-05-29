9d6d251b5a3a661989d6e45d5d6682a378959222
kan01234 <safghjkl@gmail.com>
2019-05-29 12:37:48 +0800

    scheduled tweet page init commit

--- src/main/java/com/dotterbear/file/upload/controller/TweetController.java
+++ src/main/java/com/dotterbear/file/upload/controller/TweetController.java
@@ -1,7 +1,6 @@
 package com.dotterbear.file.upload.controller;
 
 import java.text.ParseException;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -33,6 +34,11 @@ public class TweetController {
    public String listUploadedFiles(Model model) {
 //   model.addAttribute("files", uploadFileService.findAll("desc", "amendTime"));
    return "uploadForm";
+   }
+
+   @RequestMapping(value = "/scheduled-tweet", method = RequestMethod.GET)
+   public String listScheduledTweet(Model model) {
+     return "scheduledTweet";
    }
 
   @PostMapping("/add-tweet")
